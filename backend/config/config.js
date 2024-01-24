d27d2ccb1cd8e360b4d4209da95f917f9ac328e3
gabrielschmitt1 <gabrielschmitt7@gmail.com>
2024-01-24 19:23:43 -0300

    disable logging

--- backend/config/config.js
+++ backend/config/config.js
@@ -5,6 +5,7 @@ module.exports = {
     database: 'tournament',
     host: '127.0.0.1',
     dialect: 'postgres',
+    "logging": false,
   },
   // Adicione outras configurações conforme necessário
 };
